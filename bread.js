35e9ec1773595b9df91f6ed7f0259390cbaaa101
Waamber <amberastuart@aol.com>
2017-09-12 14:42:41 -0500

    added selected ingredient name to innerHTML when button is clicked

--- bread.js
+++ bread.js
@@ -1,6 +1,12 @@
 var SandwichMaker = (function (maker) {
-	const breadPrices = { "White": 0.50, "Wheat": 0.99, "Rye": 0.75, "Croissant": 1.00, "Gluten-Free Flatbread": 1.00, "Lettuce Wrap": 1.00 };
-	let selectedBread = [];
+	const breadPrices = {
+		"White": 0.50,
+		"Wheat": 0.99,
+		"Rye": 0.75,
+		"Croissant": 1.00,
+		"Gluten-Free Flatbread": 1.00,
+		"Lettuce Wrap": 1.00
+	};
 
 	maker.addBread = function (selectedBread) {
 		return breadPrices[selectedBread];
--- veggies.js
+++ veggies.js
@@ -1,6 +1,15 @@
 var SandwichMaker = (function (maker) {
-	const veggiePrices = { "Spinach": 0.50, "Arugula": 0.50, "Tomatoes": 0.75, "Lettuce": 0.25, "Onions": 0.75, "Peppers": 0.75, "Cucmbers": 0.75, "Avocado": 1.25, "No Veg": 0 };
-	let selectedVeggie = [];
+	const veggiePrices = {
+		"Spinach": 0.50,
+		"Arugula": 0.50,
+		"Tomatoes": 0.75,
+		"Lettuce": 0.25,
+		"Onions": 0.75,
+		"Peppers": 0.75,
+		"Cucmbers": 0.75,
+		"Avocado": 1.25,
+		"No Veg": 0
+	};
 
 	maker.addVeggie = function (selectedVeggie) {
 		return veggiePrices[selectedVeggie];
